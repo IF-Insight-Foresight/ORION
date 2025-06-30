13936844184b989f44d19c9414e9a194770eed33
soeirocarvalho <paulo@ifforesight.com>
2025-06-30 11:25:59 +0100

    Delete orion directory

--- orion/callbacks/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Aggregate application callbacks."""
-
-# Import modules that define callbacks so that Dash registers them when this
-# package is imported by ``run.py``.
-from .. import auth   # noqa: F401
-from .. import pages  # noqa: F401
-from . import info     # noqa: F401
-
--- orion/callbacks/info.py
+++
@@ -1,9 +0,0 @@
-from dash import Output, Input, callback
-from .. import app
-
-
-@callback(Output('info-placeholder', 'children'), Input('info-btn', 'n_clicks'))
-def show_info(n):
-    if not n:
-        return ''
-    return f'Info clicked {n}'
