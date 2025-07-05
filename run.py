f5e65dd3f48caec73b06219376b5f9735a45a710
soeirocarvalho <paulo@ifforesight.com>
2025-07-05 23:53:45 +0100

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-from orion import app, server, db
-import orion.callbacks  # noqa: F401 ensures callbacks are registered
-
-if __name__ == '__main__':
-    with server.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', port=8050, debug=False)
